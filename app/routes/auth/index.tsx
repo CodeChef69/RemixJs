c9ae0af909c3114e73031f0f1dec36e3bf13c651
CodeChef69 <gourab@microbution.com>
2023-11-11 10:00:55 +0530

    first commit

---
+++ app/routes/auth/index.tsx
@@ -0,0 +1,10 @@
+import SignIn from "components/authentication/signin";
+import { Fragment } from "react";
+
+export default function Auth() {
+return(
+    <Fragment>
+        <SignIn />
+    </Fragment>
+)
+}
